1253059251f3c431f42f2ca8e25fa9a4e13ebce7
Ayegba John <ayegbajohn@gmail.com>
2025-01-04 15:10:18 +0100

    feat:minor

---
+++ src/app/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import styles from "./Hero.module.css";
+
+const hero = () => {
+  return (
+    <div>
+      <section className={styles.hero}>
+        <div className={styles.hero__wrap}>
+          <div className={styles.hero__left}></div>
+          <div className={styles.hero__right}></div>
+        </div>
+      </section>
+      <div className={styles.feedback}>
+        <div className={styles.feedback__warp}>
+          <div className={styles.feedback__top}></div>
+          <div className={styles.feedback__buttom}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default hero;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import styles from "./page.module.css";
+import Hero from "./components/Hero/Hero";
 import Low from "./components/Low/Low";
 
 const page = () => {
@@ -24,18 +25,7 @@ const page = () => {
           </div>
         </div>
       </nav>
-      <section className={styles.hero}>
-        <div className={styles.hero__wrap}>
-          <div className={styles.hero__left}></div>
-          <div className={styles.hero__right}></div>
-        </div>
-      </section>
-      <div className={styles.feedback}>
-        <div className={styles.feedback__warp}>
-          <div className={styles.feedback__top}></div>
-          <div className={styles.feedback__buttom}></div>
-        </div>
-      </div>
+      <Hero />
       <Low />
     </div>
   );
